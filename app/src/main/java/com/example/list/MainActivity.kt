4526a25a06317ea7dcb1ffe74c7dc1c4cd1f3ccb
sergeyesp <sergey_esp@i.ua>
2023-09-27 10:03:09 +0300

    Lesson3.1

--- app/src/main/java/com/example/list/MainActivity.kt
+++ app/src/main/java/com/example/list/MainActivity.kt
@@ -2,15 +2,26 @@ package com.example.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.list.adapter.UserAdapter
 import com.example.list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var adapter: UserAdapter
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initial()
+    }
+
+    private fun initial() {
+        recyclerView = binding.rvUser
+        adapter = UserAdapter()
+        recyclerView.adapter = adapter
     }
 }
